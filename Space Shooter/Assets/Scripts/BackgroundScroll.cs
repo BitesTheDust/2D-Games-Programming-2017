e7c2f1274665a45c88ae2b4f7377bedbf3de25a1
BitesTheDust <2jaybel@gmail.com>
2017-10-14 16:58:34 +0300

    Added scrolling background

---
+++ Space Shooter/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class BackgroundScroll : MonoBehaviour
+    {
+        [SerializeField]
+        private float _speed;
+
+        private Renderer _renderer;
+
+        private void Awake()
+        {
+            _renderer = GetComponent<Renderer>();
+        }
+
+        void Update()
+        {
+            float y = Mathf.Repeat(Time.time * _speed, 1);
+            Vector2 offset = new Vector2(0, y);
+            _renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+        }
+    }
+}
